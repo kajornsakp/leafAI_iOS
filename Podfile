3e7a118fe238902c1f41a4c16dde58765578e1c4
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2018-03-14 00:18:54 +0700

    [+] Init project

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'leafAI' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'ALCameraViewController'
+  # Pods for leafAI
+
+  target 'leafAITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'leafAIUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ leafAI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leafAI
+//
+//  Created by Kajornsak Peerapathananont on 14/3/2561 BE.
+//  Copyright © 2561 Kajornsak Peerapathananont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
